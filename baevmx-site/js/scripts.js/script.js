15c501e5846777624143ef4fd04ab0124879eb6b
empresabaev <empresabaev@gmail.com>
2025-05-08 22:52:18 -0600

    Add files via upload

---
+++ baevmx-site/js/scripts.js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const destino = document.querySelector(this.getAttribute('href'));
+    destino.scrollIntoView({ behavior: 'smooth' });
+  });
+});
